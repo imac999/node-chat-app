af0c2c39185e6fb7925795fd93ec08b65f711b35
Iain Macaulay <iain_macaulay@hotmail.com>
2018-08-24 15:52:42 +0100

    added connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 //require('./config/config');
-
+const http=require('http');
+const socketIO=require('socket.io');
 const path=require('path');
 const publicPath=path.join(__dirname, '../public');
 
@@ -10,14 +11,27 @@ const bodyParser=require('body-parser');
 
 //var {authenticate} = require('./middleware/authenticate');
 
-const app=express();
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 //const port = process.env.PORT;
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('new user connected');
+
+  socket.on('disconnect', (socket) => {
+    console.log('user disconnected from server');
+  });
+  
+});
+
+
+
+server.listen(port, () => {
   console.log(`started on port: ${port}`);
 });
