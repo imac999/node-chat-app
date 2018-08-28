3052975aa2fca12a7f311ae58d4e2f918b6d9d2b
Iain Macaulay <iain_macaulay@hotmail.com>
2018-08-28 18:33:33 +0100

    scroll to btm

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,19 @@
 var socket = io();
 
+function scrollToBottom(){
+  var messages=$('#messages');
+  var newMessage=messages.children('li:last-child');
+  var clientHeight=messages.prop('clientHeight');
+  var scrollTop=messages.prop('scrollTop');
+  var scrollHeight=messages.prop('scrollHeight');
+  var newMessageHeight=newMessage.innerHeight();
+  var lastMessageHeight=newMessage.prev().innerHeight();
+
+  if((clientHeight + scrollTop + newMessageHeight + lastMessageHeight) >= scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function() {
   console.log('connected to server');
 });
@@ -17,6 +31,7 @@ socket.on('newMsg', function(msg) {
     createdAt: formattedTime
   });
   $('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLoc', function(loc) {
@@ -30,6 +45,7 @@ socket.on('newLoc', function(loc) {
     createdAt: formattedTime
   });
   $('#messages').append(html);
+  scrollToBottom();
 });
 
 
